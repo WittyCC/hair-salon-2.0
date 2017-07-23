ff58b2d723433b2596eebb28d398c778465338c3
Witty Chang <wcc1213@gmail.com>
2017-07-23 00:02:10 -0700

    Create clients table in database, override equals method in Client class.

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,6 +1,34 @@
 public class Client {
+  private String name;
+  private String contactInfo;
+  private int stylistId;
 
-  public Client(String name, int stylistId) {
+  public Client(String name, String contactInfo, int stylistId) {
+    this.name = name;
+    this.contactInfo = contactInfo;
+    this.stylistId = stylistId;
   }
 
+  public String getName() {
+    return name;
+  }
+
+  public String getContactInfo() {
+    return contactInfo;
+  }
+
+  public int getStylistId() {
+    return stylistId;
+  }
+
+  @Override
+  public boolean equals(Object otherClient){
+    if (!(otherClient instanceof Client)) {
+      return false;
+    } else {
+      Client newClient = (Client) otherClient;
+      return this.getName().equals(newClient.getName()) &&
+             this.getStylistId() == newClient.getStylistId();
+    }
+  }
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -9,8 +9,33 @@ public class ClientTest {
 
   @Test
   public void monster_instantiatesCorrectly_true() {
-    Client testClient = new Client("Bubbles", 1);
+    Client testClient = new Client("Vain Valerie", "206-XXX-XXXX", 1);
     assertEquals(true, testClient instanceof Client);
   }
 
+  @Test
+  public void Client_instantiatesWithName_String() {
+    Client testClient = new Client("Vain Valerie", "206-XXX-XXXX", 1);
+    assertEquals("Bubbles", testClient.getName());
+  }
+
+  @Test
+  public void Client_instantiatesWithStylistId_int() {
+    Client testClient = new Client("Vain Valerie", "206-XXX-XXXX", 1);
+    assertEquals(1, testClient.getStylistId());
+  }
+
+  @Test
+  public void equals_returnsTrueIfNameAndStylistIdAreSame_true() {
+    Client testClient = new Client("Vain Valerie", "206-XXX-XXXX", 1);
+    Client anotherClient = new Client("Vain Valerie", "206-XXX-XXXX", 1);
+    assertTrue(testClient.equals(anotherClient));
+  }
+
+  @Test
+  public void save_returnsTrueIfDescriptionsAretheSame() {
+    Client testClient = new Client("Vain Valerie", "206-XXX-XXXX", 1);
+    testClient.save();
+    assertTrue(Client.all().get(0).equals(testClient));
+  }
 }
