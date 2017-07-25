b195163f716a3c6c2bde2ca56f041270990b642b
Witty Chang <wcc1213@gmail.com>
2017-07-24 23:13:25 -0700

    Add styles and clean up user interface.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,7 +10,7 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("stylists", Stylist.all());
+      // model.put("stylists", Stylist.all());
       // model.put("clients", request.session().attribute("clients"));
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
