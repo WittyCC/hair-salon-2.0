d26740dd24bc35131ad91033e10001028050dc6d
Witty Chang <wcc1213@gmail.com>
2017-07-25 20:27:26 -0700

    Minor edit path in App.java.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -34,7 +34,7 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("/stylists-new", (request, response) -> {
+    get("/stylists/new", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/stylist-form.vtl");
       return new ModelAndView(model, layout);
